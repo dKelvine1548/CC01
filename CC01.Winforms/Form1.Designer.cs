a8b8f13fe69e9a9195b3948b66687302d70542dd
dKelvine1548 <71010229+dKelvine1548@users.noreply.github.com>
2020-11-18 11:18:11 +0100

    ajout du reportViewer.rdlc

--- CC01.Winforms/Form1.Designer.cs
+++ CC01.Winforms/Form1.Designer.cs
@@ -177,9 +177,9 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = " ";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
