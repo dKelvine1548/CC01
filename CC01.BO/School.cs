0975486d61c388a5beda173bf83024dd4e2ccc7e
cindyderin27 <61082408+cindyderin27@users.noreply.github.com>
2020-11-19 14:26:59 +0100

    creation de formulaire student et school

---
+++ CC01.BO/School.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC01.BO
+{
+    public class School
+    {
+        public string NameSchool { get; set; }
+        public string Email { get; set; }
+        public long SchoolNumber { get; set; }
+        public byte[] Logo { get; set; }
+
+        public School(string nameSchool, string email, long schoolNumber, byte[] logo)
+        {
+            NameSchool = nameSchool;
+            Email = email;
+            SchoolNumber = schoolNumber;
+            Logo = logo;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is School school &&
+           NameSchool.Equals(school.NameSchool, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override int GetHashCode()
+        {
+            return -251229326 + EqualityComparer<string>.Default.GetHashCode(NameSchool);
+        }
+    }
+}
---
+++ CC01.BO/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC01.BO
+{
+    public class Student
+    {
+        public string RegisterNumber { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string PlaceBirth { get; set; }
+        public long Telephone { get; set; }
+        public string Email { get; set; }
+        public byte[]Picture { get; set; }
+        public string NameSchool{ get; set; }
+
+        public Student(string registerNumber, string name, string surname,
+            DateTime birthDay, string placeBirth, long telephone, string email, byte[] picture, string nameSchool)
+        {
+            RegisterNumber = registerNumber;
+            Name = name;
+            Surname = surname;
+            BirthDay = birthDay;
+            PlaceBirth = placeBirth;
+            Telephone = telephone;
+            Email = email;
+            Picture = picture;
+            NameSchool = nameSchool;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Student student &&
+                 RegisterNumber.Equals(student.RegisterNumber, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override int GetHashCode()
+        {
+            return 73200889 + EqualityComparer<string>.Default.GetHashCode(RegisterNumber);
+        }
+    }
+}
